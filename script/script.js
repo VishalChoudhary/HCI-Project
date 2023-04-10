09baaba4baba8ce0bca45a980e02e2e9d0e3f05e
RajeswarSharma <rjqwer47@gmail.com>
2023-04-11 03:45:27 +0530

    optimized

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
 let speakerId = null;
 let textConf = null;
 const synth = window.speechSynthesis;
-
-
+let elemIds = ["s1", "s2", "s3", "s4", "s5", "s6"];
 
 const handOverHoverGeneral = async (elem) => {
     if ("speechSynthesis" in window) {
         if ((speakerId === null || speakerId !== elem.id)) {
-            console.log(speakerId);
             speakerId = elem.id;
             if (textConf === null) {
                 let response = await fetch("script/text.conf.json");
@@ -16,13 +14,12 @@ const handOverHoverGeneral = async (elem) => {
             }
             synth.cancel();
             speaker(textConf[elem.id].hover);
-            speakerId=null;
+            speakerId = null;
         }
     }
     else alert("Browser does not support text to speech");
 };
 const speaker = (speechInstance) => {
-    console.log(speechInstance)
     synth.speak(speechInstance);
 };
 
@@ -32,36 +29,17 @@ const prepareVoices = () => {
         console.log("Preparing voices");
         for (const [key, value] of Object.entries(textConf)) {
             let hoverMessage = new SpeechSynthesisUtterance();
-            hoverMessage.text =  textConf[key].hover;
-            hoverMessage.voice =  voice;
+            hoverMessage.text = textConf[key].hover;
+            hoverMessage.voice = voice;
             textConf[key].hover = hoverMessage;
         }
         console.log("Finished preparing voices");
     }
 };
 
-let s1 = document.getElementById("s1");
-let s2 = document.getElementById("s2");
-let s3 = document.getElementById("s3");
-let s4 = document.getElementById("s4");
-let s5 = document.getElementById("s5");
-let s6 = document.getElementById("s6");
-
-s1.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s1);
-});
-s2.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s2);
-});
-s3.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s3);
-});
-s4.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s4);
-});
-s5.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s5);
-});
-s6.addEventListener("mouseover", () => {
-    handOverHoverGeneral(s6);
-});
+for (const id of elemIds) {
+    let elem = document.getElementById(id);
+    elem.addEventListener("mouseover", () => {
+        handOverHoverGeneral(elem);
+    });
+}
