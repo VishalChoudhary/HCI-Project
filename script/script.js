8e58ed018e10b8b69be233ad5205e105ef81da94
VishalChoudhary <vishalghn953@gmail.com>
2023-05-19 23:27:06 +0530

    new ui update

--- script/script.js
+++ script/script.js
@@ -43,3 +43,72 @@ for (const id of elemIds) {
         handOverHoverGeneral(elem);
     });
 }
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
+
+/* Please ❤ this if you like it! */
+
+var temps = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var valElement = document.querySelector('.channel__value');
+var msgElement = document.querySelector('.channel__message');
+let channelElement = document.querySelector('.channel');
+
+let getRandomTemp = function () {
+    let channel = {
+        temp: 0,
+        message: null
+    };
+    channel.temp = (Math.random() * Math.floor(Math.random() * temps.length) * 10).toFixed(1);
+    if (channel.temp < 40) {
+        channelElement.classList.remove('channel--heated');
+        channelElement.classList.remove('channel--overheated');
+        channelElement.classList.add('channel--cooled');
+        channel.message = 'CloudVPS#1337 is <b>cooled</b> ❄️';
+    } else if (channel.temp > 40 && channel.temp < 60) {
+        channelElement.classList.remove('channel--cooled');
+        channelElement.classList.remove('channel--overheated');
+        channelElement.classList.add('channel--heated');
+        channel.message = 'CloudVPS#1337 is <b>heated</b>! ⚡'
+    } else if (channel.temp > 60) {
+        channelElement.classList.remove('channel--cooled');
+        channelElement.classList.remove('channel--heated');
+        channelElement.classList.add('channel--overheated');
+        channel.message = 'CloudVPS#1337 is <b>overheated</b>! 🔥';
+    }
+
+    return channel;
+}
+
+setInterval(function () {
+    let channel = getRandomTemp();
+    valElement.innerHTML = channel.temp;
+    msgElement.innerHTML = channel.message;
+}, 800)
